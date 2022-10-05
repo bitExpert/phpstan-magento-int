88c026b8f29f064594fe0c763595296f37602599
Stephan Hochdörfer <S.Hochdoerfer@bitExpert.de>
2022-10-05 21:42:13 +0200

    Add src dir for the module to test

---
+++ src/BitExpert_PHPStanTests/registration.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * BitExpert
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the bitexpert.de license that is
+ * available through the world-wide-web at this URL:
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    BitExpert
+ * @package     BitExpert_Quote
+ * @copyright   Copyright (c) 2021 BitExpert All rights reserved.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'BitExpert_PHPStanTests',
+    __DIR__
+);
